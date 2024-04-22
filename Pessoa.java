320ac90e8f00c34450078ef3e52a81f5cc0b9f35
ribmarciojr <marciojunior2109@gmail.com>
2024-04-22 19:45:05 -0300

    feat: sobrecarga para inicializar sem data de nascimento

--- Pessoa.java
+++ Pessoa.java
@@ -9,7 +9,7 @@ public abstract class Pessoa {
     private final String naturalidade;
     private String telefone;
     private Endereco endereco;
-    private final Date dataDeNascimento;
+    private Date dataDeNascimento;
 
     public Pessoa(String nomeCivil, String nomeSocial, String cpf, String nomeDoPai, String nomeDaMae, String naturalidade, String telefone, Endereco endereco, Date dataDeNascimento) {
         this.nomeCivil = Objects.requireNonNull(nomeCivil, "O nome civil nao pode ser nulo");
@@ -24,5 +24,19 @@ public abstract class Pessoa {
         this.telefone = Objects.requireNonNull(telefone, "O telefone nao pode ser nulo");
         this.endereco = Objects.requireNonNull(endereco, "O endereco nao pode ser nulo");
         this.dataDeNascimento = Objects.requireNonNull(dataDeNascimento, "A data de nascimento não pode ser nula");
+    }
+    
+    public Pessoa(String nomeCivil, String nomeSocial, String cpf, String nomeDoPai, String nomeDaMae, String naturalidade, String telefone, Endereco endereco) {
+        this.nomeCivil = Objects.requireNonNull(nomeCivil, "O nome civil nao pode ser nulo");
+        this.nomeSocial = Objects.requireNonNull(nomeSocial, "O nome social nao pode ser nulo");
+        if (nomeSocial.isBlank()) {
+            this.nomeSocial = this.nomeCivil;
+        }
+        this.cpf = Objects.requireNonNull(cpf, "O CPF nao pode ser nulo");
+        this.nomeDoPai = Objects.requireNonNull(nomeDoPai, "O nome do pai nao pode ser nulo");
+        this.nomeDaMae = Objects.requireNonNull(nomeDaMae, "O nome da mae nao pode ser nulo");
+        this.naturalidade = Objects.requireNonNull(naturalidade, "A naturalidade nao pode ser nula");
+        this.telefone = Objects.requireNonNull(telefone, "O telefone nao pode ser nulo");
+        this.endereco = Objects.requireNonNull(endereco, "O endereco nao pode ser nulo");
     }
 }
