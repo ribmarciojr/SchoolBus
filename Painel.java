e7c059ce7f035d5064f71b2b3f1e0e3078dd0c75
Leonardo Alakija <leoalak8@gmail.com>
2024-08-22 11:51:47 -0300

    adição das opções que faltavam ao menu

--- Painel.java
+++ Painel.java
@@ -28,7 +28,8 @@ public class Painel {
         System.out.println("[6] Ponto de Parada");
         System.out.println("[7] Rota");
         System.out.println("[8] Fornecedor");
-        System.out.println("[9] Voltar ao menu anterior");
+        System.out.println("[9] Contrato associado a um fornecedor");
+        System.out.println("[10] Voltar ao menu anterior");
     }
 
     public void mostrarLeituras(){
@@ -36,7 +37,15 @@ public class Painel {
         System.out.println("[1] Demanda de uma rota específica");
         System.out.println("[2] Numero de rotas criadas");
         System.out.println("[3] Numero de pontos de parada criados");
-        System.out.println("[4] Voltar ao menu anterior");
+        System.out.println("[4] Informações detalhadas de um aluno");
+        System.out.println("[5] Informações detalhadas de um motorista");
+        System.out.println("[6] Informações detalhadas de uma escola");
+        System.out.println("[7] Informações detalhadas de um fornecedor");
+        System.out.println("[8] Exibir o tipo de um aluno");
+        System.out.println("[9] Exibir o tipo de um motorista");
+        System.out.println("[10] Exibir o tipo de uma escola");
+        System.out.println("[11] Exibir o tipo de um fornecedor");
+        System.out.println("[12] Voltar ao menu anterior");
     }
 
     public void opcaoInvalida(){
